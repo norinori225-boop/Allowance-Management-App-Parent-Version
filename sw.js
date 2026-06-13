4b869285dd08d7278bbbcca54d0453e8777e223c
norinori225-boop <sotoma225@gmail.com>
2026-06-13 16:59:21 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = "okozukai-v1";
+const CORE = ["./", "./index.html", "./manifest.webmanifest"];
+const ICONS = ["./icon-192.png", "./icon-512.png", "./icon-maskable-512.png", "./apple-touch-icon.png"];
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(CORE).then(() => { ICONS.forEach((i) => c.add(i).catch(() => {})); })).catch(() => {}));
+  self.skipWaiting();
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(caches.match(e.request).then((hit) => hit || fetch(e.request)));
+});
